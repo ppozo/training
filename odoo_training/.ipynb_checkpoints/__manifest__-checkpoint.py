61f7769296562eded1a945d49726085057f1a48c
ppozo <patricio.pozo@gmail.com>
2020-12-09 19:23:09 +0000

    [ADD] Basic Module

---
+++ odoo_training/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo space mission',
+    'summary': """Odoo space mission> Create a module that wil plan and help Odoo get to the Moon."""',
+    'description': """
+        Odoo Inc. is trying to visit the moon, The need your help to create an App to organize the logistics.
+        This includes the Spaceship and Space Crew.
+            - P1 Create a Module that will plan and help Odoo get to the Moon.
+            
+    """,
+    
+    
+    'author': 'Odoo'
+    
+    'category': 'Training
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo': [
+        
+        
+    ],
+}
