49944cd1499c0aea9f4519b2b37df42979171c77
ppozo <patricio.pozo@gmail.com>
2020-12-16 16:29:27 +0000

    [ADD] Basic Module

---
+++ odoo_space/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odo Space',
+    'summary': """ Odoo Space App to manage spacflights """,
+    
+    'description': """
+        Odoo Space App to manage spacflights: 
+           - Ships
+           - Flishts
+           - Astronauts
+    
+    """,
+    
+    'autor': 'Odoo',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Spaceflights',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+        
+    ],
+    
+    'demo':[
+        
+        
+    ],
+        
+}
